d80ccabbcb9baad9c1c91e4fba8694d6ccf87090
MattDSquared <matthew.d.davis@gmail.com>
2015-08-22 00:07:20 -0700

    first attempt at pca + glm model, unsuccessful

--- ActivityQualityExploration.R
+++ ActivityQualityExploration.R
@@ -25,10 +25,50 @@ rawdat.train <- read.table(filepath.train, header=TRUE, sep=",", quote="\"",
 rawdat.test <- read.csv(filepath.test)
 
 ## Data Cleaning
-keepvars <- colSums(is.na(rawdat.train)) < .5*nrow(rawdat.train)
+keepvars <- colSums(is.na(rawdat.train)) < .1*nrow(rawdat.train)
 training <- rawdat.train[,keepvars]
 
+training <- mutate(training, 
+                   timestamp = training$raw_timestamp_part_1 + 
+                       training$raw_timestamp_part_2/1e6, 
+                   new_window = factor(new_window), 
+                   user_name = factor(user_name))
+
+dummies <- dummyVars(classe ~ user_name + new_window, data=training)
+
+training <- cbind(training, predict(dummies, newdata=training))
+
+training <- select(training, 
+                   -user_name, 
+                   -raw_timestamp_part_1, 
+                   -raw_timestamp_part_2,
+                   -cvtd_timestamp,
+                   -new_window)
+       
 ## subset training into local test and train data
-inTrain <- createDataPartition(training$classe, p=.6, list=FALSE)
+inTrain <- createDataPartition(training$classe, p=.1, list=FALSE)
 training <- training[inTrain,]
 quizing <- training[-inTrain,]
+
+## data exploration
+gg <- qplot(timestamp, num_window, 
+            data=filter(training,timestamp<1322600000))
+print(gg)
+
+gg <- qplot(timestamp, pitch_belt, data=filter(training,timestamp<1322600000))
+gg <- gg + geom_point(aes(timestamp, roll_belt), colour="blue")
+gg <- gg + geom_point(aes(timestamp, yaw_belt), colour="green")
+print(gg)
+
+gg <- qplot(timestamp, accel_belt_x, data=filter(training,timestamp<1322600000))
+gg <- gg + geom_point(aes(timestamp, accel_belt_y), colour="blue")
+gg <- gg + geom_point(aes(timestamp, accel_belt_z), colour="green")
+print(gg)
+
+train.pca <- prcomp(select(training, -classe), center=TRUE, scale=TRUE)
+print(summary(train.pca))
+
+## Preprocessing
+pre.pca <- preProcess(select(training, -classe), method="pca", thresh=.95)
+train.pca <- predict(pre.pca, select(training, -classe))
+model <- train(training$classe ~ ., method="glm", data=train.pca)
